f76f2259c551246324d4d2734012bb059314327e
youssef el ouidad <y.elouidad@gmail.com>
2023-06-26 23:16:39 +0100

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Produces output according to a format
+ * @format: The format string
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					count += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
+/**
+ * _puts - Writes a string to stdout
+ * @str: The string to write
+ *
+ * Return: The number of characters written
+ */
+int _puts(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+
+	return (count);
+}
