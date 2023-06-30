94754979ef1f82b9f4f26a36a370e94fc669287e
youssef el ouidad <y.elouidad@gmail.com>
2023-06-30 23:16:35 +0100

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ main_va.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    count = print_format(format, args);
+
+    va_end(args);
+
+    return count;
+}
+
